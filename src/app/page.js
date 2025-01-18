2a669bc1b4d44858f177fd750575a1bf830bca45
isha-bagadiya <ishabagadiya@lampos.tech>
2025-01-18 15:37:08 +0530

    loader added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
 import About from "./components/About";
 import Footer from "./components/Footer";
 import GetStarted from "./components/GetStarted";
@@ -5,11 +8,38 @@ import HeroSection from "./components/HeroSection";
 import SupportedChains from "./components/SupportedChains";
 import Usecase from "./components/Usecase";
 import Walkthrough from "./components/Walkthrough";
+import Image from "next/image";
 // import Script from "next/script";
 // import Head from "next/head";
 
-
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="bg-black flex items-center justify-center w-full h-[100vh] overflow-hidden">
+        <div className="w-[200px] h-max overflow-hidden">
+        <Image
+          src="/loader.gif"
+          alt="Loading..."
+          width={400}
+          height={400}
+          className="w-auto h-auto object-contain"
+          priority
+        />
+          </div>
+      </div>
+    );
+  }
+
   return (
     <>
       {/* <Script id="googleanalytics_page"
@@ -32,7 +62,6 @@ export default function Home() {
         <title>Welcome!</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head> */}
-
 
       <HeroSection />
       <SupportedChains />
